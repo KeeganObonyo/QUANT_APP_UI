517c71259fa99e27ebcfd346f8ed73042c289c51
keegan <keeganobonyo@gmail.com>
2018-08-22 15:57:28 +0300

    improvements for the login functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,7 +13,7 @@ export class LoginComponent {
 
   public loginError = false;
   public loggedIn = false;
-  public credential = { 'email': '', 'password': ''};
+  public credential = { 'email': '', 'password': '' };
 
   public password: string;
   public email: string;
@@ -24,23 +24,37 @@ export class LoginComponent {
 
   user: any;
 
+  cannot_be_blank: boolean;
+
   constructor(public http: Http, public router: Router, public route: ActivatedRoute, public log: LoginService) { }
 
   onLogin() {
-    console.log(this.credential);
+    localStorage.clear();
     this.log.getUserToken(this.credential).subscribe(
       res => {
         localStorage.setItem('token', 'JWT ' + res.json().token);
         this.loggedIn = true;
-        console.log(localStorage.getItem('token'));
-        this.router.navigate(['']);
+        if (localStorage.getItem('token')) {
+          this.router.navigate(['']);
+        } else {
+          this.router.navigate(['/signup']);
+        }
       },
       error => {
-        console.log(error);
+        this.router.navigate(['/signup']);
         this.loggedIn = false;
         this.loginError = true;
       }
     );
   }
 
+  validate() {
+
+    if (this.credential.password && this.credential.email) {
+      this.cannot_be_blank = false;
+      this.onLogin();
+    } else {
+      this.cannot_be_blank = true;
+    }
+  }
 }
